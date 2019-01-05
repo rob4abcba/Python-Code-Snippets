c7d167a306de60c4c136fffff65c55a316455d21
Rob <rob_abcba@yahoo.com>
2019-01-05 08:47:59 -0800

    Updated intro.py

--- Python-Imports/intro.py
+++ Python-Imports/intro.py
@@ -1,2 +1,10 @@
 
+import my_module as mm
+
 courses = ['History', 'Math', 'Physics', 'CompSci']
+
+#index = my_module.find_index(courses, 'Math')
+#print(index)
+
+index = mm.find_index(courses, 'Math')
+print(index)
